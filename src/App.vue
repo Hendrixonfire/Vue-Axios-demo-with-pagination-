1c446032ff6a0e736f30abdc7039f0fc1613c2d4
Hendrixonfire <129618415+Hendrixonfire@users.noreply.github.com>
2023-11-29 00:59:35 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+      <transaction-paginated 
+      :data="trans"
+      :total-pages = "Math.ceil(trans.length) / 5"
+      :total = "trans.length"
+      :per-page = "5"
+      :current-page="currentPage"
+      @pagechanged = "onPagechanged"
+      />
+  </div>
+</template>
+
+
+
+<script>
+import TransactionPaginated from '../src/components/Paginator.vue'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'app',
+mounted(){
+  this.loadTrans()
+},
+
+data(){
+  return{
+    currentPage: 1,
+   
+   
+  }
+},
+methods:{
+  ...mapActions(['loadTrans']),
+  onPagechanged(page){
+    this.currentPage = page;
+  }
+
+},
+computed:{
+  ...mapGetters([
+    'trans',
+    'getTransById'
+  ])
+},
+
+
+components: {
+  TransactionPaginated
+}
+}
+</script>
+
+
+
+<style >
+
+</style>
